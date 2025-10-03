6fdd640223faef2d69c9b17ad9cf7a8d4b358b8c
MatthewRSouth <matthewrogersouth@gmail.com>
2025-10-04 01:02:46 +0900

    finished modal window

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,18 @@
 
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
-
+const ctaModal = document.querySelector('.cta-modal');
+const ctaModalBtn = document.querySelector('.cta-button');
+const closeModalBtn = document.querySelector('.close-modal-btn');
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
+});
+
+ctaModalBtn.addEventListener('click', () => {
+    ctaModal.classList.remove('hidden');
+});
+
+closeModalBtn.addEventListener('click', () => {
+    ctaModal.classList.add('hidden');
 });

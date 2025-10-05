9a5646cb8edef2905b410b1cd5436b0d11779214
MatthewRSouth <matthewrogersouth@gmail.com>
2025-10-05 20:58:17 +0900

    added slide show

--- public/js/index.js
+++ public/js/index.js
@@ -67,3 +67,17 @@ ctaModal.addEventListener('click', function (e) {
 });
 
 ///////////////slideshow
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});

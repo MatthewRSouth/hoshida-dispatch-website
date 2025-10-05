56961eaf6592470b18bfdd06cebae9745be27328
MatthewRSouth <matthewrogersouth@gmail.com>
2025-10-05 14:52:55 +0900

    fixed navigation and made header sticky

--- public/js/index.js
+++ public/js/index.js
@@ -5,12 +5,42 @@ const offScreenMenu = document.querySelector('.off-screen-menu');
 const ctaModal = document.querySelector('.cta-modal');
 const ctaModalBtn = document.querySelectorAll('.cta-button');
 const closeModalBtn = document.querySelector('.close-modal-btn');
+const introductionSection = document.querySelector('.introduction-container');
+const introductionBtn = document.querySelector('.scroll-to-introduction');
+const testimonialSection = document.querySelector('.testimonial-container');
+const testimonialBtn = document.querySelector('.scroll-to-testimonials');
+const staffSection = document.querySelector('.staff-container');
+const staffBtn = document.querySelector('.scroll-to-staff');
+const pricingSection = document.querySelector('.pricing-container');
+const pricingBtn = document.querySelector('.scroll-to-pricing');
+const contactSection = document.querySelector('.hero-container');
+const contactBtn = document.querySelector('.scroll-to-contact');
 /////////////hamburger menu
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 });
-
+/////////////scrolling
+introductionBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    introductionSection.scrollIntoView({ behavior: 'smooth' });
+});
+testimonialBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    testimonialSection.scrollIntoView({ behavior: 'smooth' });
+});
+staffBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    staffSection.scrollIntoView({ behavior: 'smooth' });
+});
+pricingBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    pricingSection.scrollIntoView({ behavior: 'smooth' });
+});
+contactBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    contactSection.scrollIntoView({ behavior: 'smooth' });
+});
 //////////////Modal window
 
 function closeModal() {

32f730229fb3acfe96a4c2b4266a4ae87c1344fb
MatthewRSouth <matthewrogersouth@gmail.com>
2025-10-05 13:40:19 +0900

    added phone number, pictures, and styling

--- public/js/index.js
+++ public/js/index.js
@@ -3,17 +3,37 @@
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 const ctaModal = document.querySelector('.cta-modal');
-const ctaModalBtn = document.querySelector('.cta-button');
+const ctaModalBtn = document.querySelectorAll('.cta-button');
 const closeModalBtn = document.querySelector('.close-modal-btn');
+/////////////hamburger menu
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 });
 
-ctaModalBtn.addEventListener('click', () => {
-    ctaModal.classList.remove('hidden');
+//////////////Modal window
+
+function closeModal() {
+    ctaModal.classList.add('hidden');
+}
+ctaModalBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        ctaModal.classList.remove('hidden');
+    });
 });
 
 closeModalBtn.addEventListener('click', () => {
-    ctaModal.classList.add('hidden');
+    closeModal();
 });
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+        closeModal();
+    }
+});
+ctaModal.addEventListener('click', function (e) {
+    if (e.target === ctaModal) {
+        closeModal();
+    }
+});
+
+///////////////slideshow
